db46c8f9a2bacc75432282fe5a840c21c002aa7c
christinamcmahon <christina.mcmahon1@gmail.com>
2019-12-30 12:35:50 -0800

    Delete app folder

--- app/models/review.rb
+++
@@ -1,4 +0,0 @@
-class Review < ActiveRecord::Base
-  belongs_to :User
-  belongs_to :BoardGame
-end
--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ActiveRecord::Base
-  has_many :reviews
-  has_many :board_games, through: :reviews
-end
